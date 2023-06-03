df7707fb89bb4cc577df8ab31e230eee7163ebf4
Ar-Ray-code <ray255ar@gmail.com>
2023-06-03 16:27:14 +0900

    add thread

--- src/main.rs
+++ src/main.rs
@@ -26,42 +26,36 @@ use embedded_graphics::{
     text::{Baseline, Text},
 };
 use ssd1306::{prelude::*, Ssd1306};
+
 mod fmtbuf;
 use fmtbuf::FmtBuf;
 
+// thread setup
+use cortex_m::peripheral::syst::SystClkSource;
+use cortexm_threads::{init, create_thread, create_thread_with_config, sleep};
 
-#[entry]
-fn main() -> ! {
-    const XTAL_FREQ_HZ: u32 = 12_000_000u32;
-    const PWM_50_HZ: u8 = 20u8;
+// global variables
+static mut ADC_READ: u16 = 0u16;
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+const PWM_50_HZ: u8 = 20u8;
+
+
+fn mythread_1 () -> ! {
+    info!("Hello from thread 1");
 
     // Clock setup ====================================
-    let mut pac = pac::Peripherals::take().unwrap();
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-
-    let clocks = hal::clocks::init_clocks_and_plls(
-        XTAL_FREQ_HZ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
+    let mut my_pac = pac::Peripherals::take().unwrap();
+    let mut my_watchdog = hal::Watchdog::new(my_pac.WATCHDOG);
+    let my_clocks = hal::clocks::init_clocks_and_plls(XTAL_FREQ_HZ, my_pac.XOSC, my_pac.CLOCKS, my_pac.PLL_SYS, my_pac.PLL_USB, &mut my_pac.RESETS, &mut my_watchdog).ok().unwrap();
 
     // GPIO setup ====================================
-    let sio = hal::Sio::new(pac.SIO);
-    let pins = rp_pico::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
+    let sio = hal::Sio::new(my_pac.SIO);
+    let pins = rp_pico::Pins::new(my_pac.IO_BANK0, my_pac.PADS_BANK0, sio.gpio_bank0, &mut my_pac.RESETS);
+
+
 
     // Delay setup ====================================
-    let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+    let timer = hal::Timer::new(my_pac.TIMER, &mut my_pac.RESETS);
     let mut delay = timer.count_down();
 
     // LED setup ====================================
@@ -73,7 +67,7 @@ fn main() -> ! {
 
 
     // ADC setup ====================================
-    let mut adc = hal::Adc::new(pac.ADC, &mut pac.RESETS);
+    let mut adc = hal::Adc::new(my_pac.ADC, &mut my_pac.RESETS);
     let mut adc_pin_0 = pins.gpio26.into_floating_input();
 
     // SSD1306 setup ====================================
@@ -81,12 +75,12 @@ fn main() -> ! {
     let scl_pin = pins.gpio17.into_mode::<hal::gpio::FunctionI2C>();
 
     let i2c = hal::I2C::i2c0(
-        pac.I2C0,
+        my_pac.I2C0,
         sda_pin,
         scl_pin,
         400.kHz(),
-        &mut pac.RESETS,
-        &clocks.peripheral_clock,
+        &mut my_pac.RESETS,
+        &my_clocks.peripheral_clock,
     );
 
     let interface = ssd1306::I2CDisplayInterface::new(i2c);
@@ -94,10 +88,10 @@ fn main() -> ! {
     display.init().unwrap();
 
     let text_style = MonoTextStyleBuilder::new().font(&FONT_9X18_BOLD).text_color(BinaryColor::On).build();
-    let mut buf = FmtBuf::new();
+
 
     // Servo setup ====================================
-    let mut pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+    let mut pwm_slices = hal::pwm::Slices::new(my_pac.PWM, &mut my_pac.RESETS);
     let pwm = &mut pwm_slices.pwm7;
     pwm.set_ph_correct();
 
@@ -107,17 +101,17 @@ fn main() -> ! {
     channel.output_to(pins.gpio15);
     info!("PWM setup done");
 
-    // Main loop ====================================
+    let mut buf = FmtBuf::new();
+    buf.write_str("Hello Rust!").unwrap();
+
     loop {
         buf.reset();
         display.clear();
 
-        let pin_adc_counts: u16 = adc.read(&mut adc_pin_0).unwrap();
-
-        info!("ADC: {}", pin_adc_counts);
-
-        // servo output
-        let duty: u16 = (4095u16 - pin_adc_counts) * 4u16 + 2500u16;
+        unsafe {
+            ADC_READ = adc.read(&mut adc_pin_0).unwrap();
+        }
+        let duty: u16 = (4095u16 - unsafe { ADC_READ }) * 4u16 + 2500u16;
         channel.set_duty(duty);
 
         // led output
@@ -137,5 +131,37 @@ fn main() -> ! {
         delay.start(500.millis());
         nb::block!(delay.wait()).unwrap();
     }
+
 }
 
+fn mythread_2 () -> ! {
+    info!("Hello from thread 2");
+    loop {
+        unsafe {
+            info!("ADC: {}", ADC_READ);
+        }
+        sleep(1000);
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let mut syst = cp.SYST;
+
+    syst.set_clock_source(SystClkSource::Core);
+    syst.set_reload(80_000);
+    syst.enable_counter();
+    syst.enable_interrupt();
+
+	let mut stack1 = [0xDEADBEEF; 512];
+    let mut stack2 = [0xDEADBEEF; 512];
+
+    let _ = create_thread(
+        &mut stack1,
+        mythread_1
+    );
+    let _ = create_thread(&mut stack2, mythread_2);
+
+    init();
+}
